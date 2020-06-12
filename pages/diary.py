a1c2c92a7ebae38e24d2afd0918e0c65bf259ffe
Blake Aber <blake.aber@gmail.com>
2020-06-12 16:57:45 -0400

    hooked up diary entries

--- pages/diary.py
+++ pages/diary.py
@@ -8,7 +8,8 @@ from app import (
     State,
     ClientsideFunction,
     sql,
-    time
+    time,
+    json
     )
 
 
@@ -51,7 +52,7 @@ pain_increase_checklist = dbc.FormGroup(
         dbc.Checklist(
             id="pain-increase-checklist",
             options=[
-                {"label": i, "value": idx} for idx, i in enumerate(pain_areas)
+                {"label": i, "value": i} for i in pain_areas
             ],
             switch=True,
             inline=False
@@ -66,7 +67,7 @@ pain_decrease_checklist = dbc.FormGroup(
         dbc.Checklist(
             id="pain-decrease-checklist",
             options=[
-                {"label": i, "value": idx} for idx, i in enumerate(pain_areas)
+                {"label": i, "value": i} for i in pain_areas
             ],
             switch=True,
             inline=False
@@ -100,6 +101,7 @@ layout = dbc.Container([
     ], justify="center"),
     dbc.Row([    
         dbc.Col([
+            html.Div(id='diary-submit-status'),
             dbc.Button("Submit", id='diary-submit-button', color="primary", block=True)
         ], 
         width=12),
@@ -112,4 +114,40 @@ layout = dbc.Container([
 def disable_tabs_while_recording(value):
     if value:
         return value
+
+
+
+@app.callback(Output('diary-submit-status', 'children'),
+              [Input('diary-submit-button', 'n_clicks')],
+              [State('user-id', 'data'),
+               State('pain-slider', 'value'),
+               State('pain-increase-checklist', 'value'),
+               State('pain-decrease-checklist', 'value')])
+def save_diary_to_sql(n_clicks, user, pain_level, getting_better, getting_worse):
+    if n_clicks:
+        unixtime = time.time()
+        user_hash = user['user-hash']
+        data = {
+            "pain_level": pain_level,
+            "getting_better": getting_better,
+            "getting_worse": getting_worse
+        }
+        
+        if not data['pain_level']:  # pain must be filled in
+            return html.H5(
+                'Please fill in all data fields to continue :)', 
+                style={'text-align': 'center', 'color': 'red'}
+            )
+
+        payload = json.dumps(data)
+        sql_statement = f"""
+           INSERT INTO pain_levels (user_hash, pain, unixtime) 
+           VALUES ('{user_hash}', '{payload}', {unixtime})
+           """
+        sql.insert(sql_statement)
+        return html.H5(
+            'Got it - thanks so much!', 
+            style={'text-align': 'center', 'color': 'green'}
+        )
+
 
