4c2f324c0effaedfeef72bc3b89d59c231ee15c4
Blake Aber <blake.aber@gmail.com>
2020-06-15 19:12:20 -0400

    working onboarding, with blocked progression; need to NOT submit pain until all elements filled out

--- onboarding/steps.py
+++ onboarding/steps.py
@@ -16,9 +16,9 @@ from pages import profile, diary, exercise
 
 INPUT_STEPS = [
     'Welcome',
-    'Health Profile',
-    'Pain Assessment',
-    'Exercise Program'
+    'Profile',
+    'Pain Level',
+    'Exercises'
 ]
 
 
@@ -30,7 +30,8 @@ def create_process_steps_from_list(input_steps):
     step_width = 12 / total_steps
     return [
         dbc.Tab(
-            tab_id=step_idx+1,
+            id=f'onboard-tab-{step_idx+1}',
+            tab_id=f'onboard-{step_idx+1}',
             label=step_text,
             label_style={
                 'border': 'none',
@@ -47,6 +48,8 @@ welcome = html.P('Welcome!!!')
 
 layout = dbc.Container([
     html.Br(),
+    dcc.Store(id='profile-complete', storage_type='memory'),
+    dcc.Store(id='diary-complete', storage_type='memory'),
     dbc.Tabs(id='onboarding-steps', children=create_process_steps_from_list(INPUT_STEPS)),
     dbc.Row([
         dbc.Col(dbc.Progress(
@@ -57,8 +60,8 @@ layout = dbc.Container([
             animated=True
         ))
     ]),
-    html.Br(),
-    html.Div(id='onboarding-content'),
+    html.Div(id='onboarding-submit-status'),
+    html.Div(id='onboarding-content')
 ])
 
 
@@ -66,16 +69,98 @@ layout = dbc.Container([
                Output('progress-steps', 'value')],
               [Input('onboarding-steps', 'active_tab')])
 def display_page(active_tab):
-    if not active_tab or (active_tab == 1):
-        return welcome, 15
-    elif active_tab == 2:
-        return profile.layout, 30
-    elif active_tab == 3:
-        return diary.layout, 50
-    elif active_tab == 4:
-        return welcome, 80
+    if not active_tab or (active_tab == 'onboard-1'):
+        return welcome, 10
+    elif active_tab == 'onboard-2':
+        return profile.onboard_layout, 32
+    elif active_tab == 'onboard-3':
+        return diary.onboard_layout, 55
+    elif active_tab == 'onboard-4':
+        return welcome, 78
 #         return exercise.layout
     else:
         return html.P('This indicates an error has occured :(')
+
+
+@app.callback(Output('profile-complete', 'data'),
+              [Input('url', 'pathname'),
+               Input('sex-radio', 'value'),
+               Input('height-slider', 'value'),
+               Input('weight-slider', 'value'),
+               Input('activity-slider', 'value'),
+               Input('age-slider', 'value'),
+               Input('personal-experience-text', 'value'),
+               Input('previous-pt-radio', 'value')],
+              [State('user-id', 'data')])
+def save_profile_to_sql(pathname, sex, height, weight, activity, age, experience, pt, user):
+    is_profile_complete = False
+
+    data = {
+        "sex": sex,
+        "height": height,
+        "weight": weight,
+        "activity": activity,
+        "age": age,
+        "experience": experience,
+        "previous_pt": pt
+    }
+
+    if user and all(data.values()):  # all values must be filled in
+        unixtime = time.time()
+        user_hash = user['user-hash']
+        data['experience'] = data['experience'].replace("'", "''")            
+
+        payload = json.dumps(data)
+        sql_statement = f"""
+           INSERT INTO profiles (user_hash, profile, unixtime) 
+           VALUES ('{user_hash}', '{payload}', {unixtime})
+           """
+        sql.insert(sql_statement)
+
+        is_profile_complete = True
+    
+    return is_profile_complete
+
+
+@app.callback(Output('diary-complete', 'data'),
+              [Input('pain-slider', 'value'),
+               Input('pain-increase-checklist', 'value'),
+               Input('pain-decrease-checklist', 'value')],
+              [State('user-id', 'data'),
+               State('profile-complete', 'data')])
+def save_diary_to_sql(pain_level, getting_better, getting_worse, user, profile_complete):
+    is_diary_complete = False
+
+    if not profile_complete:
+        return is_diary_complete
+    else:
+        data = {
+            "pain_level": pain_level,
+            "getting_better": getting_better,
+            "getting_worse": getting_worse
+        }
+
+        if (data['pain_level'] is not None) and (data['pain_level'] >= 0):  # pain must be filled in
+            unixtime = time.time()
+            user_hash = user['user-hash']
+
+            payload = json.dumps(data)
+            sql_statement = f"""
+               INSERT INTO pain_levels (user_hash, pain, unixtime) 
+               VALUES ('{user_hash}', '{payload}', {unixtime})
+               """
+            sql.insert(sql_statement)
+
+            is_diary_complete = True
+    
+    return is_diary_complete
+
+
+@app.callback([Output('onboard-tab-3', 'disabled'),
+               Output('onboard-tab-4', 'disabled')],
+              [Input('profile-complete', 'data'),
+               Input('diary-complete', 'data')])
+def save_diary_to_sql(profile_complete, diary_complete):
+    return not profile_complete, not diary_complete
 
 
